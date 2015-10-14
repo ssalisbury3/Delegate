1464b05b624915719b7cb66a64c650ee07048cbd
ssalisbury3 <ssalisbury3@butlercc.edu>
2015-10-13 20:12:53 -0500

    my delegate example

---
+++ Module08_Delegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate string Del(string letter);
+namespace Module08_Delegate
+{
+    class Program
+    {
+        static string word = "";
+        public static string AddCharacter(string letter)
+        {
+            word = word + letter;
+            return word;
+        }
+        public static string DeleteLastCharacter(string letter)
+        {
+            word = word.Remove(word.Length - 1);
+            return word;
+        }
+        public static string UpperCharacter(string letter)
+        {
+            word = word.ToUpper();
+            return word;
+        }
+        public static void DelegateMethod(string letter)
+        {
+            Console.WriteLine(word);
+        }
+        static void Main()
+        {
+            Del del = new Del(AddCharacter);
+            word = del("h");
+            del("i");
+            del("!");
+            Del del1 = new Del (UpperCharacter);
+            word = del1("");
+            Del del2 = new Del(DeleteLastCharacter);
+            word = del2("");
+            Console.WriteLine("{0}", word);
+            Console.ReadKey();
+        }
+
+    }
+}
+
